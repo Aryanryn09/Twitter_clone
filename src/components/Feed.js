94ef109e0ba0be04901bf73a63c62658d63f6cb2
Aryan Bansod <aryanem09@gmail.com>
2023-04-16 22:43:44 +0530

    commit1

---
+++ src/components/Feed.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import './Feed.css'
+
+function Feed() {
+  return (
+// ??header
+
+ <div>
+    <h1>Home</h1>
+    <div className="for">
+    <h2 className ='Foryou'>For you</h2>
+    <h2 className='following'>Following</h2> 
+    </div>
+   </div>
+
+
+// Tweetbox
+// post
+// post
+// post
+// post
+// post
+// post
+// post
+   
+
+
+// Posts
+
+
+
+  )
+}
+
+export default Feed
